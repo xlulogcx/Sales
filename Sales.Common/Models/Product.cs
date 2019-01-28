3620a3961909fda658db54f1fe5aefe2f1b3c7b2
xlulogcx <l_g30@hotmail.com>
2019-01-28 07:56:00 -0600

    Otros

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Decimal price { get; set; } 
+        public bool IsAvailable { get; set; }
+        public DateTime PublishOn { get; set; }
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
